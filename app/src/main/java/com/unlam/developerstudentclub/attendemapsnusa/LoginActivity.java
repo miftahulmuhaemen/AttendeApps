d29ead565551a2fd4ba24d758191c95f514e6f8c
miftahulmuhaemen <miftahulmuhaemen@gmail.com>
2019-01-10 05:54:32 +0800

    Menu

--- app/src/main/java/com/unlam/developerstudentclub/attendemapsnusa/LoginActivity.java
+++ app/src/main/java/com/unlam/developerstudentclub/attendemapsnusa/LoginActivity.java
@@ -1,16 +1,26 @@
 package com.unlam.developerstudentclub.attendemapsnusa;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.marcinorlowski.fonty.Fonty;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class LoginActivity extends AppCompatActivity {
+
+    @BindView(R.id.btn_login)
+    Button btn_login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
 
         Fonty
                 .context(this)
@@ -19,5 +29,13 @@ public class LoginActivity extends AppCompatActivity {
                 .build();
 
         Fonty.setFonts(this);
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this,PresensiActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/unlam/developerstudentclub/attendemapsnusa/PresensiActivity.java
+++ app/src/main/java/com/unlam/developerstudentclub/attendemapsnusa/PresensiActivity.java
@@ -29,73 +29,71 @@ import pub.devrel.easypermissions.EasyPermissions;
 
 public class PresensiActivity extends AppCompatActivity {
 
-    @BindView(R.id.btn_permission)
-    Button btn_permission;
-    @BindView(R.id.text_permission)
-    TextView text_permission;
-
-    static int RC_LOCATION_PERMISSION_CODE = 101;
+//    @BindView(R.id.btn_permission)
+//    Button btn_permission;
+//    @BindView(R.id.text_permission)
+//    TextView text_permission;
+//
+//    static int RC_LOCATION_PERMISSION_CODE = 101;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_presensi);
+        setContentView(R.layout.activity_attende);
 
         ButterKnife.bind(this);
-        btn_permission.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                methodRequiresTwoPermission();
-            }
-        });
-        methodRequiresTwoPermission();
+//        btn_permission.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                methodRequiresTwoPermission();
+//            }
+//        });
+//        methodRequiresTwoPermission();
     }
 
-    private void methodRequiresTwoPermission() {
-        String[] perms = {Manifest.permission.ACCESS_FINE_LOCATION};
-        if (EasyPermissions.hasPermissions(this, perms)) {
-            btn_permission.setText("Check In");
-            text_permission.setText("Silahan Check in");
-            btn_permission.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    locationPoint();
-                }
-            });
-        } else {
-            // Do not have permissions, request them now
-            EasyPermissions.requestPermissions(this,
-                    "Izin lokasi?",
-//                    getString(R.string.camera_and_location_rationale),
-                    RC_LOCATION_PERMISSION_CODE, perms);
-        }
-    }
-
-    public void locationPoint() {
-
-        LocationRequest request = new LocationRequest();
-        request.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-        FusedLocationProviderClient client = LocationServices.getFusedLocationProviderClient(this);
-        int permission = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
-
-        if (permission == PackageManager.PERMISSION_GRANTED) {
-            client.requestLocationUpdates(request, new LocationCallback() {
-
-                @Override
-                public void onLocationResult(LocationResult locationResult) {
-
-                    Location location = locationResult.getLastLocation();
-
-                    if (location != null) {
-                        Location company = new Location("");
-                        company.setLatitude(-3.4016452);
-                        company.setLongitude(115.9044867);
-                        Toast.makeText(PresensiActivity.this, Math.round(location.distanceTo(company)) + "meter", Toast.LENGTH_SHORT).show();
-                    }
-
-                }
-            }, null);
-        }
-    }
+//    private void methodRequiresTwoPermission() {
+//        String[] perms = {Manifest.permission.ACCESS_FINE_LOCATION};
+//        if (EasyPermissions.hasPermissions(this, perms)) {
+//            btn_permission.setText("Check In");
+//            text_permission.setText("Silahan Check in");
+//            btn_permission.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View view) {
+//                    locationPoint();
+//                }
+//            });
+//        } else {
+//            EasyPermissions.requestPermissions(this,
+//                    "Izin lokasi?",
+//                    RC_LOCATION_PERMISSION_CODE, perms);
+//        }
+//    }
+//
+//    public void locationPoint() {
+//
+//        LocationRequest request = new LocationRequest();
+//        request.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+//        FusedLocationProviderClient client = LocationServices.getFusedLocationProviderClient(this);
+//        int permission = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
+//
+//        if (permission == PackageManager.PERMISSION_GRANTED) {
+//            client.requestLocationUpdates(request, new LocationCallback() {
+//
+//                @Override
+//                public void onLocationResult(LocationResult locationResult) {
+//
+//                    Location location = locationResult.getLastLocation();
+//
+//                    if (location != null) {
+//                        Location company = new Location("");
+//                        company.setLatitude(-3.4016452);
+//                        company.setLongitude(115.9044867);
+//                        Toast.makeText(PresensiActivity.this, Math.round(location.distanceTo(company)) + "meter", Toast.LENGTH_SHORT).show();
+//                    }
+//
+//                }
+//            }, null);
+//        }
+//    }
 
 }
